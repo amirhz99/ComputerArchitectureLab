1566a1b24ca3ffadb15cfd165eb8b9476d96c90b
Amir Hosein Zarei <amirhz9978@gmail.com>
2020-11-22 00:36:23 +0330

    pc risc_v

---
+++ pc.v
@@ -0,0 +1,14 @@
+
+module pc(  clk , in , out , reset ,write);
+input clk , reset,write;
+input [63:0] in ;
+output  reg [63:0]  out;
+always @(posedge clk)
+begin
+    if(reset)
+        out = 0 ;
+    else
+       if (write)
+          out = in ;
+end
+endmodule;
